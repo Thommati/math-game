0128c385ff92ad52337ab2f6d16392b959371c53
Matthew Thompson <supra__charged@hotmail.com>
2021-06-21 11:28:58 -0600

    Write Player class

--- Player.rb
+++ Player.rb
@@ -1,8 +1,17 @@
 class Player
   attr_reader :player_name
+  attr_reader :lives
 
   def initialize player_name
     @player_name = player_name
+    @lives = 3
+  end
+
+  def decrement_lives
+    @lives -= 1
+  end
+
+  def reset_lives
     @lives = 3
   end
 end
